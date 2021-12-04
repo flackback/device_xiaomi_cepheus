d834a59305d36d25f7cf871997b2df128d04ffc3
logan.humbert <logan2.humbert@gmail.com>
2021-12-04 21:23:33 +0100

    Nuke pixel thermal/power hals

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -164,6 +164,9 @@ TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
+# Power
+TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
+
 # QCOM
 BOARD_USES_QCOM_HARDWARE := true
 
